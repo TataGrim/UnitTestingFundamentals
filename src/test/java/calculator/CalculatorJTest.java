a44e96278817b31859f909e3615712fbb1508ab8
cosmi <baciucosmin64@gmail.com>
2025-03-08 14:09:15 +0200

    created unit test for Calculator with AssertJ

---
+++ src/test/java/calculator/CalculatorJTest.java
@@ -0,0 +1,32 @@
+package calculator;
+
+import org.example.calculator.Calculator;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class CalculatorJTest {
+
+    private final Calculator calculatorUT=new Calculator();
+
+    @Test
+    public void testSubstract(){
+        assertThat(calculatorUT.substract(-2,2))
+                .isEqualTo(-4);
+        assertThat(calculatorUT.substract(2,-2)==4)
+                .isTrue()
+                .isInstanceOf(Calculator.class);
+
+    }
+
+    @Test
+    public void testDivide(){
+        assertThatThrownBy(()-> {
+            calculatorUT.divide(2, 0);
+        })
+                .isInstanceOf(ArithmeticException.class)
+                .hasMessage("Divide by zero");
+        }
+    }
+
